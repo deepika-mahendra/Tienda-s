58224a5b8482ad9ab993a94b70425cbb630f4de3
vtf05 <56218201+vtf05@users.noreply.github.com>
2020-10-15 15:43:54 +0530

    Update models.py
    Added new model

--- mac/mac/shop/models.py
+++ mac/mac/shop/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50,blank=True, null=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+        
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    native_name = models.CharField(max_length=5)
+    phone_no=models.CharField(max_length=10)
+    dob = models.DateField()
+    address = models.CharField(max_length=255)
+    vehicle= models.CharField(max_length=50)
+    vehicle_location=models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip = models.CharField(max_length=5)
+    photo = models.ImageField(upload_to='uploads', blank=True)
+    
+
+
+
 
 # Create your models here.
 class Product(models.Model):
